bbf35162029cd627724a21ad3d03635aae2b66c3
Dinh Thi Le <dinhthile1711@gmail.com>
2026-05-02 11:58:48 +0700

    remove webdrivermanager use selenium manager

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -3,7 +3,6 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
@@ -11,38 +10,28 @@ public class DriverFactory {
 
         if (browser.equalsIgnoreCase("chrome")) {
 
-            // ✅ FIX CỨNG VERSION (tránh mismatch trên CI)
-        	WebDriverManager.chromedriver()
-            .driverVersion("147.0.0")
-            .setup();
-        	
             ChromeOptions options = new ChromeOptions();
 
-            // 🔥 detect môi trường CI
             boolean isCI = System.getenv("CI") != null;
 
             if (isCI) {
-                // ===== CHẠY TRÊN CI/CD =====
                 options.addArguments("--headless=new");
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
                 options.addArguments("--window-size=1920,1080");
             } else {
-                // ===== CHẠY LOCAL =====
                 options.addArguments("--start-maximized");
             }
 
-            // 🔽 common options (áp dụng cho mọi môi trường)
             options.addArguments("--disable-notifications");
             options.addArguments("--disable-infobars");
             options.addArguments("--disable-extensions");
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-geolocation");
-            options.addArguments("--disable-blink-features=AutomationControlled");
 
-            return new ChromeDriver(options);
+            return new ChromeDriver(options); // 🔥 Selenium Manager tự xử lý
         }
 
-        throw new RuntimeException("Browser not supported: " + browser);
+        throw new RuntimeException("Browser not supported");
     }
 }
